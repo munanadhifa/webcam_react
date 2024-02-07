50eeb9c97e5debe30a8f300aad34a3889fb0633d
Muna Nadhifa Ridwan <mnadhifa@prixa.ai>
2024-02-07 14:58:46 +0700

    add capture button & capture pic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import "./input.css";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 const WebcamToy = () => {
   const webcamRef = useRef(null);
+  const [captureImage, setCaptureImage] = useState(null);
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCaptureImage(imageSrc);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="relative">
-        <Webcam audio={false} ref={webcamRef} className={`w-full h-auto `} />
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          className={`w-full h-auto rounded-md mt-4`}
+          mirrored={true}
+        />
+        <button
+          onClick={capture}
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 "
+        >
+          Capture
+        </button>
+        <div>
+          {captureImage && (
+            <img
+              src={captureImage}
+              alt="capture"
+              className="mt-4 w-65 h-40 rounded-md mb-4"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
