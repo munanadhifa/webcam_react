a9a7f6a66598c3856f11e9bf86cccfc6fd5d3af9
Muna Nadhifa Ridwan <mnadhifa@prixa.ai>
2024-02-07 15:19:13 +0700

    add css for capture pic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./input.css";
-
 import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 const WebcamToy = () => {
   const webcamRef = useRef(null);
-  const [captureImage, setCaptureImage] = useState(null);
+  const [captureImage, setCaptureImage] = useState([]);
+  const [imageCount, setImageCount] = useState(0);
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
-    setCaptureImage(imageSrc);
+    setCaptureImage([...captureImage, imageSrc]);
+    setImageCount(imageCount + 1);
   };
 
   return (
@@ -18,24 +19,25 @@ const WebcamToy = () => {
         <Webcam
           audio={false}
           ref={webcamRef}
-          className={`w-full h-auto rounded-md mt-4`}
+          className={`w-full h-auto  mt-4`}
           mirrored={true}
         />
-        <button
-          onClick={capture}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 "
-        >
-          Capture
-        </button>
-        <div>
-          {captureImage && (
-            <img
-              src={captureImage}
-              alt="capture"
-              className="mt-4 w-65 h-40 rounded-md mb-4"
-            />
-          )}
-        </div>
+      </div>
+      <button
+        onClick={capture}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 "
+      >
+        Capture
+      </button>
+      <div className="flex overflow-x-scroll w-[700px] ">
+        {captureImage.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Captured ${index}`}
+            className="mt-4 w-65 h-40 pr-2.5  mb-4"
+          />
+        ))}
       </div>
     </div>
   );
